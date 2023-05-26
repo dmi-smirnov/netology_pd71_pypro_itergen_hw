8b85262f7f55e580cf958a8ee5e275dbfe3acd40
Dmitriy Smirnov <git@d-sm.ru>
2023-05-26 16:45:45 +0300

    task3 completed

---
+++ netology_pd71_pypro_itergen_hw_task3.py
@@ -0,0 +1,52 @@
+# Необязательное задание.
+# Написать итератор, аналогичный итератору из задания 1,
+# но обрабатывающий списки с любым уровнем вложенности.
+# Шаблон и тест в коде ниже:
+
+class FlatIterator:
+
+  def __init__(self, list_of_lists):
+    self.list_of_lists = list_of_lists
+
+  def __iter__(self):
+    self.lists_iters = [self.list_of_lists.__iter__()]
+    return self
+  
+  def __next__(self):
+    item = FlatIterator.__get_next_item(self.lists_iters)
+    return item
+  
+  def __get_next_item(lists_iters):
+    try:
+      item = lists_iters[-1].__next__()
+    except StopIteration:
+      lists_iters.pop()
+      if not lists_iters:
+        raise StopIteration
+      item = FlatIterator.__get_next_item(lists_iters)
+        
+    if isinstance(item, list):
+        lists_iters.append(item.__iter__())
+        item = FlatIterator.__get_next_item(lists_iters)
+
+    return item
+
+def test_3():
+
+  list_of_lists_2 = [
+    [['a'], ['b', 'c']],
+    ['d', 'e', [['f'], 'h'], False],
+    [1, 2, None, [[[[['!']]]]], []]
+  ]
+
+  for flat_iterator_item, check_item in zip(
+      FlatIterator(list_of_lists_2),
+      ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+  ):
+
+    assert flat_iterator_item == check_item
+
+  assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+if __name__ == '__main__':
+  test_3()
