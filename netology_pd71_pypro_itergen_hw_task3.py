51452d60d461a2ab7bfa9ca83cc7ad695c94f60c
Dmitriy Smirnov <git@d-sm.ru>
2023-05-26 17:51:08 +0300

    task3 improved

--- netology_pd71_pypro_itergen_hw_task3.py
+++ netology_pd71_pypro_itergen_hw_task3.py
@@ -17,19 +17,31 @@ class FlatIterator:
     return item
   
   def __get_next_item(lists_iters):
-    try:
-      item = lists_iters[-1].__next__()
-    except StopIteration:
-      lists_iters.pop()
-      if not lists_iters:
-        raise StopIteration
-      item = FlatIterator.__get_next_item(lists_iters)
+    while lists_iters:
+      try:
+        item = lists_iters[-1].__next__()
+        if isinstance(item, list):
+          lists_iters.append(item.__iter__())
+        else:
+          return item
+      except StopIteration:
+        lists_iters.pop()
+    raise StopIteration
+    
+    # Вариант с рекурсией:
+    # try:
+    #   item = lists_iters[-1].__next__()
+    # except StopIteration:
+    #   lists_iters.pop()
+    #   if not lists_iters:
+    #     raise StopIteration
+    #   item = FlatIterator.__get_next_item(lists_iters)
         
-    if isinstance(item, list):
-        lists_iters.append(item.__iter__())
-        item = FlatIterator.__get_next_item(lists_iters)
+    # if isinstance(item, list):
+    #     lists_iters.append(item.__iter__())
+    #     item = FlatIterator.__get_next_item(lists_iters)
 
-    return item
+    # return item
 
 def test_3():
 
